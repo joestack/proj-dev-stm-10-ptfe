59c6c759ae0bc1726dfd3254d3c3543e3e5b583b
joestack <joern@joern-online.de>
2021-10-22 13:00:24 +0200

    fixed fqdn output 3

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "terraform_fqdn" {
-  value = "${aws_route53_record.tfenode.*.name}.${data.terraform_remote_state.foundation.outputs.dns_domain}"
+  value = "${aws_route53_record.tfenode.*.name[0]}.${data.terraform_remote_state.foundation.outputs.dns_domain}"
 }
