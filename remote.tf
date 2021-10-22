3647a42f9f5f71cc3223c756fce0d43aaeb9eb6b
joestack <joern@joern-online.de>
2021-10-22 13:55:30 +0200

    added 2nd WS to remote state and 1st inventory test 2

--- remote.tf
+++ remote.tf
@@ -4,7 +4,17 @@ data "terraform_remote_state" "foundation" {
   config = {
     organization = "joestack"
     workspaces = {
-      name = "proj-dev-stm-01-foundation",
+      name = "proj-dev-stm-01-foundation"
+    }
+  }
+}
+
+data "terraform_remote_state" "bastionhost" {
+  backend = "remote"
+
+  config = {
+    organization = "joestack"
+    workspaces = {
       name = "proj-dev-stm-02-bastionhost"
     }
   }
